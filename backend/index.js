8955346a4276da92f6c07cdb1cc9c77b33e3b7e6
Nehal Garg <nehalgarg37@gmail.com>
2023-08-26 23:46:28 +0530

    Veggis

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors= require('cors');
+const UserModel= require('./models/user_register');
+const ContactModel = require('./models/contact');
+
+const app= express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/User");
+
+app.post("/login",(req,res)=>{
+    const {email,password} = req.body;
+    UserModel.findOne({email:email})
+    .then(user=>{
+        if(user){
+        if(user.password === password){
+            res.json("Success")
+        }else{
+            res.json("The password is incorrect")
+        }
+    }
+        else{
+            res.json("No record found")
+        }
+    })
+})
+
+app.post('/register',(req,res)=>{
+    UserModel.create(req.body)
+    .then(nameuser=>res.json(nameuser))
+    .catch(err=> res.json(err))
+})
+
+
+app.post('/contact',(req,res)=>{
+    ContactModel.create(req.body)
+    .then(contact=>res.json(contact))
+    .catch(err=> res.json(err))
+})
+
+
+app.listen(5000,()=>{
+    console.log("Server is running")
+})
